3ce1e88ab2e9eb969637f7f753f1010608a11f58
kate-rusaleieva <kate-rusaleieva15@mail.ru>
2016-04-18 11:09:40 +0600

    game1

---
+++ Vue1.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",
+	function(){
+		new Vue({
+			el:"#game",
+			data:function(){
+				return{
+					ballx:0,
+					bally:0,
+					boardPosition:0,
+					boardWidth:100,
+					fieldWidth:600
+				}
+			},
+			methods:{
+				onMouse:function(e){
+					this.boardPosition=e.clientX-(this.boardWidth/2);
+					if(this.boardPosition<0){
+						this.boardPosition=0;
+					};
+					if(this.boardPosition+this.boardWidth>this.fieldWidth){
+						this.boardPosition=this.fieldWidth-this.boardWidth;
+					}
+				}	
+			}
+	});
+});
